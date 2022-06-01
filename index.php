f49011faf5c4aa92360e801ec85b3c1099091627
Júlia Guimarães Franco <92822185+juliaguima@users.noreply.github.com>
2022-06-01 12:30:06 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+include_once "./includes/_head.php";
+include_once "./includes/_header.php";
+?>
+
+<main>
+
+
+
+
+
+
+
+<script src="script.js"></script> 
+<?php
+
+include_once "./includes/_footer.php";
+?>
+
