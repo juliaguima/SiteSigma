826ef7e579707a2fca49d9d4d4a9d272b9a617f4
juliaguima <92822185+juliaguima@users.noreply.github.com>
2022-06-02 12:27:31 -0300

    Aula - 02.06

---
+++ sitesigma/includes/_dados.php
@@ -0,0 +1,28 @@
+<?php
+
+$produtos = array(
+
+0 => array('nome'=>'Capivara Legal','preço'=> '1200.00','descricao'=>'Descrição Capivara Legal','imagem'=>'capivara-legal.jpg'), 
+1 => array('nome'=>'Rato Feio','preço'=> '10.00','descricao'=>'Descrição Rato Feio','imagem'=>'Rato-feio.webp'), 
+2 => array('nome'=>'Capivara Legal','preço'=> '1200.00','descricao'=>'Descrição Capivara Legal','imagem'=>'capivara-legal.jpg'), 
+3 => array('nome'=>'Capivara Legal','preço'=> '1200.00','descricao'=>'Descrição Capivara Legal','imagem'=>'capivara-legal.jpg'), 
+4 => array('nome'=>'Capivara Legal','preço'=> '1200.00','descricao'=>'Descrição Capivara Legal','imagem'=>'capivara-legal.jpg'), 
+5 => array('nome'=>'Capivara Legal','preço'=> '1200.00','descricao'=>'Descrição Capivara Legal','imagem'=>'capivara-legal.jpg'), 
+6 => array('nome'=>'Capivara Legal','preço'=> '1200.00','descricao'=>'Descrição Capivara Legal','imagem'=>'capivara-legal.jpg'), 
+7 => array('nome'=>'Capivara Legal','preço'=> '1200.00','descricao'=>'Descrição Capivara Legal','imagem'=>'capivara-legal.jpg'), 
+8 => array('nome'=>'Capivara Legal','preço'=> '1200.00','descricao'=>'Descrição Capivara Legal','imagem'=>'capivara-legal.jpg'), 
+9 => array('nome'=>'Capivara Legal','preço'=> '1200.00','descricao'=>'Descrição Capivara Legal','imagem'=>'capivara-legal.jpg'), 
+
+
+
+
+
+
+
+
+
+
+
+
+)
+?>
