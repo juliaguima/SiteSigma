7592117e253b242f36c2ccdc695f00a9e665daa5
juliaguima <guimaraesfrancojulia@gmail.com>
2022-06-09 12:18:13 -0300

    Update index.php

--- sitesigma/index.php
+++ sitesigma/index.php
@@ -1,13 +1,33 @@
 <?php
-// include do footer
+
 include_once './includes/_dados.php';
 include_once './includes/_head.php';
 include_once './includes/_header.php';
+
 ?>
 
 <h1>Home</h1>
 
 <?php
-// include do footer
+
+for ($i=0; $i < 10; $i++) { 
+   echo $i. '<br>';
+
+   echo $produtos [$i] ['nome'];
+   echo $produtos [$i] ['preço'];
+   echo '<img src="./content/'.$produtos [$i] ['imagem'].'">';
+   echo '<hr>';
+
+
+}
+
+
+?>
+
+<img src="./content/<?php echo $produtos[$id]['imagem'];?>">
+
+
+<?php
+
 include_once './includes/_footer.php';
 ?>
