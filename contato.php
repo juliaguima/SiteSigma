1416165318caadf157f58b7a76eb5f230d2f52c0
juliaguima <92822185+juliaguima@users.noreply.github.com>
2022-07-07 11:46:19 -0300

    Banco de dados
    Inclusão do banco de dados.

--- contato.php
+++ contato.php
@@ -1,63 +1,71 @@
 <?php
 
+// include do footer
+
 include_once './includes/_dados.php';
+
 include_once './includes/_head.php';
+
 include_once './includes/_header.php';
 
 
-//validação do envio da variável
 
-$a = array(1);
-if (isset ($_POST['txtNome'])) {
-    $nome = ($_POST['txtNome']);
-    $email =($_POST['txtemail']);
-    $contato=($_POST['txtcontato']);
-    $mensagem =($_POST['txtmensagem']);
-    print_r($a);
+if (isset ( $_POST['txtNome']) ) {
+
+    $nome = strtoupper( $_POST['txtNome']);
+
+    $nome = $_POST['txtNome'];
+
+    $email = $_POST['txtEmail'];
+
+    $telefone = $_POST['txtTelefone'];
+
+    $mensagem = $_POST['txtMensagem'];
+
+    echo $nome. "<br>". $email."<br>". $telefone."<br>". $mensagem;
 
 }
 
 
 
-// strtoupper: caixa alta.
 ?>
-
-
 
 <h1>Contato</h1>
+<form action="./contato.php" method="post">
+<ul>
+ <li>
+ <label for="txtNome">Nome Completo</label>
+<input type="text" name="txtNome" id="txtNome">
+ </li>
+<li>
+ <label for="txtEmail">E-mail</label>
+ <input type="text" name="txtEmail" id="txtEmail">
+</li>
+<li>
+ <label for="txtTelefone">Telefone</label>
+<input type="text" name="txtTelefone" id="txtTelefone">
+</li>
+<li>
 
-        <form action = "./contato.php" method = "post">
-        <label for="txtNome">Nome Completo: </label>
-        <br>
-        <input type="text" name="txtNome" id="txtNome" required>
-        
-    </form>
-        <form action = "./contato.php" method = "post">
-        <label for="txtemail">Email: </label>
-        <br>
-        <input type="text" placeholder="mail@exemplo.com.br" />
-       
+<label for="txtMensagem">Mensagem</label>
 
-    </form>
-        <form action = "./contato.php" method = "post">
-        <label for="txtcontato">Contato: </label>
-        <br>
-        <input type="text"name="txtcontato" id="txtcontato" placeholder="(00)000000000" />
+        <input type="text" name="txtMensagem" id="txtMensagem">
+
+        </li>
+
+        <input type="submit" value="Cadastrar">
+
+    </ul>
+
+</form>
 
 
-    </form>
 
-        <label for="txtmensagem">Mensagem: </label>
-    <br>
-        <p class="text">
-        <textarea placeholder="Escreva sua mensagem" /></textarea>
-        </p>
-
-    <p class="submit">
-        <input type="submit" value="Enviar" />
-    </p>
 
 <?php
-include_once './includes/_footer.php';
-?>
 
+// include do footer
+
+include_once './includes/_footer.php';
+
+?>
